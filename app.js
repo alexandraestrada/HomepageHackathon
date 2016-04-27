1f68f4897cb32690c422f2012b9643b4a1b73bd0
alexandra estrada <estrada.ae@gmail.com>
2016-04-26 20:48:32 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express= require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.route('/login')
+	.get(function(req,res) {
+		//login form goes here for user login
+		res.send('login form goes here')
+	})
+	.post(function(req,res) {
+		res.send('processing login for validation')
+	})
+
+app.listen(3000)
+console.log('magic port on 3000')

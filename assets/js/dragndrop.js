e436fbbb9abdc3aed6af6e36c114fc4799bbc29b
Sharvil Naik <sharvil.naik@macys.com>
2016-04-27 13:47:24 -0700

    image dragging

--- assets/js/dragndrop.js
+++ assets/js/dragndrop.js
@@ -1,38 +1,55 @@
 $(function() {
-  $("li.draggable").mousedown(function() {
-    $(this).css("pointer-events", "none");
-    $(this).find("div").hide();
+  /*$("#imgList,#imgMainList").sortable({
+          connectWith: "#imgList,#imgMainList",
   });
-  $("body").mouseup(function() {
-    $("#imgDiv").find("li").css("pointer-events", "auto");
-  })
-  interact('img.draggable')
-    .draggable({
-      inertia: true,
-      autoScroll: true,
-      onmove: dragMoveListener,
-      onend: function (event) {
-        var textEl = event.target.querySelector('p');
+  $("#imgList,#imgMainList").disableSelection();*/
 
-        textEl && (textEl.textContent =
-          'moved a distance of '
-          + (Math.sqrt(event.dx * event.dx +
-                       event.dy * event.dy)|0) + 'px');
-      }
-    });
+  $( "#imgList, #imgMainList" ).sortable({
+    "placeholder": "temp",
+    "cursor": "move",
+    "connectWith": "#imgMainList, #imgList",
+    "start": ondrag,
+    "stop": ondrop
+  });
+  $( "#imgList, #imgMainList" ).disableSelection();
 
-    function dragMoveListener (event) {
-      var target = event.target,
-          // keep the dragged position in the data-x/data-y attributes
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+  function ondrag(ev, ui) {
+    ui.placeholder.animate({
+      width: ui.helper.width(),
+      height: ui.helper.height()
+    }, 1000);
+    ui.helper.animate({
+      width: ui.item.width(),
+      height: ui.item.height()
+    }, 1000, function() {alert("hi")});
+  }
 
-      target.style.webkitTransform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
+  function ondrop() {
+    //alert("hi")
+  }
 
-      // update the posiion attributes
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    };
+  // interact('img.draggable')
+  //   .draggable({
+  //     inertia: true,
+  //     autoScroll: true,
+  //     onmove: dragMoveListener,
+  //     onend: function (event) {
+  //       alert("hi");
+  //     }
+  //   });
+
+  //   function dragMoveListener (event) {
+  //     var target = event.target,
+  //         // keep the dragged position in the data-x/data-y attributes
+  //         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+  //         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  //     target.style.webkitTransform =
+  //     target.style.transform =
+  //       'translate(' + x + 'px, ' + y + 'px)';
+
+  //     // update the posiion attributes
+  //     target.setAttribute('data-x', x);
+  //     target.setAttribute('data-y', y);
+  //   };
 });

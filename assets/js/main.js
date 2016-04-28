b372f1f6e7272f31f68fca40238a56e724a80ad3
Sharvil Naik <sharvil.naik@macys.com>
2016-04-28 09:50:43 -0700

    alt tags & active image working

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,21 @@ $(function() {
 		if(e.target.tagName.toLowerCase() === "img") {
 			$("li.selected").removeClass("selected");
 			$(e.target).parent().addClass("selected");
+			$("#altInput").val($(e.target).attr("alt"));
 		}
 	});
 
-	$("#addAlt").click(function() {
-		$("li.selected").find("img").attr("alt", $("#altInput").val());
-		$("#altInput").val("");
-	})
+	$("#altInput").blur(function() {
+		$("li.selected").find("img").attr("alt", $(this).val());
+	});
+
+	// $(window).resize(function() {
+	// 	var $layout = $("#layoutDiv");
+	// 	if($("#contentDiv").width() < 960) {
+	// 		$layout.width("100%").find("li").each(function() {
+	// 			var temp = 600;
+	// 			$(this).width((($layout.width() * temp) / 960) + "px");
+	// 		});
+	// 	}
+	// }).trigger("resize");
 });

ccc45cd75e73e605b93cbd0c2cf176e76eef4319
alexandra estrada <estrada.ae@gmail.com>
2016-04-27 19:46:47 -0700

    working image upload

---
+++ assets/js/dropzone.js
@@ -0,0 +1,17 @@
+
+// console.log('it hit!')
+Dropzone.options.dzForm = {
+	init: function() {
+		this.on('success', function(file, resp) {
+			$('#fileDiv').css('display','none');
+			var imageArray = []
+			$("#dzForm").append($('<input type="hidden" ' +
+              'name="files[]" ' + 'value="' + resp.fileName + '">'))
+			console.log(file.name)
+
+			$('#imgList').append('<li><img /></li>');
+			$('#imgList li:last-child img').attr('src', './images/uploads/' + file.name)
+			
+		})
+	}
+}

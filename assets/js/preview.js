3034e55aea4f240d9dc2c367142fa35bfb28dfab
alexandra estrada <estrada.ae@gmail.com>
2016-04-28 19:41:21 -0700

    move preview js to another file

--- assets/js/preview.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function() {
-	var body = localStorage.getItem('template')
-	$('#globalContentContainer').append(body)
-})
-
-// document.getElementById("container").innerHTML = localStorage.getItem("template");
